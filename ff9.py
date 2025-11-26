0527b363180a4490038be927339d9f344a33db6b
kuznec232a-hue <kuznec232a@gmail.com>
2025-11-26 19:45:04 +0300

    Update ff9.py

--- ff9.py
+++ ff9.py
@@ -169,7 +169,7 @@ class NeuralCore:
 
         try:
             resp = self.client.chat.completions.create(
-                model="google/gemini-3-pro-preview-high",
+                model="deepseek/deepseek-chat",
                 messages=messages, temperature=0.7
             )
             return "OK", resp.choices[0].message.content
@@ -385,6 +385,7 @@ with col_dash:
         use_container_width=True
 
     )
+
 
 
 
